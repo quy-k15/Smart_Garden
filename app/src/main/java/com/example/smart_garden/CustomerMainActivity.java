1532d92e29557480758bac9e31c46925a1e0c8c4
Quý <80635501+quy-k15@users.noreply.github.com>
2023-04-24 15:59:47 +0700

    fix bug + kết nối firebase

--- app/src/main/java/com/example/smart_garden/CustomerMainActivity.java
+++ app/src/main/java/com/example/smart_garden/CustomerMainActivity.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.smart_garden.databinding.ActivityMainBinding;
 
 
 public class CustomerMainActivity extends AppCompatActivity{
 
-    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
